27c6562158aba0daa215871cd7090b7cc0cf875e
Caio Lucena Colaço <caio.lcolaco@gmail.com>
2025-11-09 22:33:25 -0300

    Update server and docker-compose

--- src/index.ts
+++ src/index.ts
@@ -37,8 +37,8 @@ app.use(helmet());
 app.use(cors({
   origin: true,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: '*',
+  methods: '*',
 }));
 
 // Middleware de logging
